d3fe55913eb272266c0f9c3a4611ca907f938f3e
Harshal <harshal0921@gmail.com>
2023-12-16 00:15:57 +0530

    added color picker, draw button and eraser

--- script.js
+++ script.js
@@ -3,12 +3,34 @@ let displayPixelValue = document.querySelector("#dimension");
 let canvas = document.querySelector("#canvas");
 let pixelValue = pixels.value;
 let clear = document.querySelector(".clear");
+let colorPicker = document.querySelector("#color-picker");
+let currentColor = colorPicker.value;
+let eraser = document.querySelector(".eraser");
+let draw = document.querySelector(".draw");
+
+draw.style.backgroundColor = "grey";
 
 pixels.addEventListener("input", () => {
   pixelValue = pixels.value;
   displayPixelValue.textContent =
     pixelValue.toString() + " x " + pixelValue.toString();
   renderPixels(pixelValue);
+});
+
+colorPicker.addEventListener("input", (event) => {
+  currentColor = event.target.value;
+});
+
+draw.addEventListener("click", () => {
+  eraser.style.backgroundColor = "";
+  draw.style.backgroundColor = "grey";
+  Sketch();
+});
+
+eraser.addEventListener("click", () => {
+  draw.style.backgroundColor = "";
+  eraser.style.backgroundColor = "grey";
+  Erase();
 });
 
 clear.addEventListener("click", Clear);
@@ -43,7 +65,16 @@ function Sketch() {
   let pixelList = document.querySelectorAll(".pixel");
   pixelList.forEach((item) => {
     item.addEventListener("mouseover", () => {
-      item.style.backgroundColor = "black";
+      item.style.backgroundColor = currentColor;
+    });
+  });
+}
+
+function Erase() {
+  let pixelList = document.querySelectorAll(".pixel");
+  pixelList.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "white";
     });
   });
 }
